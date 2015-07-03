0176916e67ee1db1a0fd1f9ae851e9c5ea0e4b06
Valjean <valjeanbud@gmail.com>
2015-07-03 15:08:02 -0700

    test(home): 2 basic tests in place

--- src/home/home.controller.spec.js
+++ src/home/home.controller.spec.js
@@ -1,22 +1,65 @@
 'use strict';
 
-describe('controllers', function () {
-	var scope;
+describe('controllers and service', function () {
+	var scope, controller, service,
+		openAssetsArray;
 
 	beforeEach(module('starterAngularBabel'));
 
-	beforeEach(inject(function ($rootScope) {
-		scope = $rootScope.$new();
+	beforeEach(inject(function ($rootScope, $controller, assetsService) {
+		scope = $rootScope.$new(),
+			controller = $controller('Home', {}),
+			service = assetsService;
 	}));
 
-	it('should define more than 5 open source assets', inject(function ($controller) {
-		expect(scope.openAssets).toBeUndefined();
 
-		$controller('Home', {
-			$scope: scope
+	describe('assets service factory', function () {
+		it('should define a function to return openAssets > 5', function () {
+			expect(service.getOpenAssets()).toBeTruthy();
+
+			openAssetsArray = service.getOpenAssets();
+
+			expect(openAssetsArray).toBeDefined();
+
+			expect(openAssetsArray.length).toBeGreaterThan(5);
 		});
+	});
 
-		expect(angular.isArray(scope.openAssets)).toBeTruthy();
-		expect(scope.openAssets.length > 5).toBeTruthy();
-	}));
+
+	it('should allow the controller to get the Open Assets', function () {
+		expect(controller).toBeTruthy();
+		expect(controller).toBeDefined();
+		expect(controller.openAssets).toBeDefined();
+
+		//openAssetsArray = the call for getOpenAssets() from service
+		controller.openAssets = openAssetsArray;
+
+		expect(controller.openAssets).toBeTruthy();
+
+		expect(controller.openAssets.length).toBeGreaterThan(5);
+	});
 });
+
+
+//standalone test for factory assetsService
+/*describe("assets service factory test", function(){
+ var scope, controller, service;
+
+ beforeEach(module('intcStarterAngularBabel'));
+
+ beforeEach(inject(function(assetsService){
+ service = assetsService;
+ }));
+
+ it('should define the return functions', function() {
+ expect(service.getIntcAssets()).toBeTruthy();
+ expect(service.getIntcAssets()).toBeDefined();
+
+ expect(service.getOpenAssets()).toBeTruthy();
+ expect(service.getOpenAssets()).toBeDefined();
+ });
+ }); */
+
+
+
+
